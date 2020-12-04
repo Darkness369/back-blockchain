bb50a47920d18b03ba3e397a5cd279cb2ae1eeb9
Darkness369 <52568166+Darkness369@users.noreply.github.com>
2020-12-03 23:31:01 -0700

    Update mongoDBAtlas.js

--- config/mongoDBAtlas.js
+++ config/mongoDBAtlas.js
@@ -6,7 +6,7 @@ require('dotenv').config({ path: 'variables.env' });
 const conectarDB = async () => {
     
     try {
-        await mongoose.connect(process.env.DB_MONGO,{
+        await mongoose.connect("mongodb+srv://faceless:o5v7tuqIpChp3TdH@cluster0.erp3c.mongodb.net/blockhain?retryWrites=true",{
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
